6223d9f950da0aa5c19744e15dcf24c1591719a3
Alexander Tortsev <ecialo@yandex.ru>
2026-04-06 18:12:36 +0400

    cleanup

--- app/Main.hs
+++ app/Main.hs
@@ -10,14 +10,6 @@ module Main where
 
 import Lib
 import Miso
-import Miso.CSS (StyleSheet)
-import Miso.CSS qualified as CSS
-import Miso.Html.Element as H
-import Miso.Html.Event as E
-import Miso.Html.Property as P
-import Miso.Lens
-import Miso.String
-import Paths_homepage qualified as HomepagePaths
 
 -----------------------------------------------------------------------------
 data Action
@@ -33,12 +25,6 @@ foreign export javascript "hs_start" main :: IO ()
 #endif
 #endif
 -----------------------------------------------------------------------------
--- main :: IO ()
--- #ifdef INTERACTIVE
--- main = reload defaultEvents app
--- #else
--- main = startApp defaultEvents app
--- #endif
 
 main :: IO ()
 #ifdef INTERACTIVE
@@ -54,50 +40,3 @@ main = do
     app <- makeCalculatorApp
     startApp defaultEvents app
 #endif
------------------------------------------------------------------------------
--- app :: App Int Action
--- app =
---     (component 0 updateModel viewModel)
---         { styles = [Sheet sheet]
---         }
-
--- -----------------------------------------------------------------------------
--- updateModel :: Action -> Effect parent Int Action
--- updateModel = \case
---     AddOne ->
---         this += 1
---     SubtractOne ->
---         this -= 1
---     SayHelloWorld ->
---         io_ (consoleLog "Hello World!")
-
--- -----------------------------------------------------------------------------
--- viewModel :: Int -> View Int Action
--- viewModel x =
---     H.div_
---         [P.class_ "counter-container"]
---         [ H.h1_
---             [ P.class_ "counter-title"
---             ]
---             [ "🍜 Miso sampler"
---             ]
---         , H.div_
---             [ P.class_ "counter-display"
---             ]
---             [ text (ms x)
---             ]
---         , H.div_
---             [ P.class_ "buttons-container"
---             ]
---             [ H.button_
---                 [ E.onClick AddOne
---                 , P.class_ "decrement-btn"
---                 ]
---                 [text "+"]
---             , H.button_
---                 [ E.onClick SubtractOne
---                 , P.class_ "increment-btn"
---                 ]
---                 [text "-"]
---             ]
---         ]
